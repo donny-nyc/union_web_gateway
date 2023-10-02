04e803dc54e8c91b97bcac6df74a03857ce4ddfe
Stephen Donahue <sdonahue.metacomet@gmail.com>
2023-10-02 18:20:23 +0100

    wip: http client

--- src/infra/utils/http_client.ts
+++ src/infra/utils/http_client.ts
@@ -13,27 +13,34 @@ export enum HttpHeader {
 };
 
 export interface HttpClientI<T> {
-  get(host: string, port: number, path: string, headers?: HttpHeaders): Promise<T>;
-  post(host: string, port: number, path: string, headers?: HttpHeaders): Promise<T>;
-  put(host: string, port: number, path: string, headers?: HttpHeaders): Promise<T>;
-  delete(host: string, port: number, path: string, headers?: HttpHeaders): Promise<T>;
+  get(host: string, port: number, path: string): Promise<T>;
+  post(host: string, port: number, path: string, body?: any): Promise<T>;
+  put(host: string, port: number, path: string, body?: any): Promise<T>;
+  delete(host: string, port: number, path: string): Promise<T>;
 };
 
 type HttpClientOptions = {
   host: string,
   port: number,
   path: string,
-  verb: HttpVerb,
+  method: HttpVerb,
   headers: {
     "Content-Type": string,
-    "Content-Size": number,
+    "Content-Size"?: number,
   }
 };
 
 export class HttpClient<T> {
   get(host: string, port: number, path: string): Promise<T> {
     return new Promise<T>((resolve) => {
-      const req = http.request(this.prepareOptions(HttpVerb.GET), (res) => {
+      const options = this.prepareOptions(
+        host,
+        port,
+        path,
+        HttpVerb.GET
+      );
+
+      const req = http.request(options, (res) => {
         let data: any = []
 
         if(res.statusCode !== 200) {
@@ -65,9 +72,27 @@ export class HttpClient<T> {
     });
   }
 
-  post(host: string, port: number, path: string, body: any): Promise<T> {
+  post(
+    host: string, 
+    port: number, 
+    path: string, 
+    body: any
+  ): Promise<T> {
     return new Promise<T>((resolve) => {
-      const req = http.request(this.prepareOptions(HttpVerb.POST), (res) => {
+      const postData: string = JSON.stringify(body);
+
+      const contentLength: number = Buffer.byteLength(postData);
+
+      const options = this.prepareOptions(
+        host,
+        port,
+        path,
+        HttpVerb.POST,
+        contentLength
+      );
+
+      const req 
+        = http.request(options, (res) => {
         let data: any = []
 
         if(res.statusCode !== 200) {
@@ -79,20 +104,21 @@ export class HttpClient<T> {
         });
 
         res.on('end', () => {
-          console.log('Order found');
-
           console.log(Buffer.concat(data).toString());
 
           const result: T = JSON.parse(Buffer.concat(data).toString())
 
-          console.log('[Orders] GET result: ', result);
+          console.log('POST results:', result);
 
           resolve(result);
         });
       });
 
       try {
-        req.write(post_data);
+        if (contentLength > 0) {
+          req.write(postData);
+        }
+
         req.end();
       } catch (e: any) {
         console.error(e);
@@ -100,7 +126,125 @@ export class HttpClient<T> {
     });
   }
 
-  private prepareOptions(verb: HttpVerb, headers?: HttpHeaders): HttpClientOptions {
+  put(
+    host: string, 
+    port: number, 
+    path: string, 
+    body: any
+  ): Promise<T> {
+    return new Promise<T>((resolve) => {
+      const putData: string = JSON.stringify(body);
 
+      const contentLength: number = Buffer.byteLength(putData);
+
+      const options = this.prepareOptions(
+        host,
+        port,
+        path,
+        HttpVerb.PUT,
+        contentLength
+      );
+
+      const req 
+        = http.request(options, (res) => {
+        let data: any = []
+
+        if(res.statusCode !== 200) {
+          throw new Error("failed to put");
+        }
+
+        res.on('data', chunk => {
+          data.push(chunk);
+        });
+
+        res.on('end', () => {
+          console.log(Buffer.concat(data).toString());
+
+          const result: T = JSON.parse(Buffer.concat(data).toString())
+
+          console.log('PUT results:', result);
+
+          resolve(result);
+        });
+      });
+
+      try {
+        if (contentLength > 0) {
+          req.write(putData);
+        }
+
+        req.end();
+      } catch (e: any) {
+        console.error(e);
+      }
+    });
+  }
+
+  delete(host: string, port: number, path: string): Promise<T> {
+    return new Promise<T>((resolve) => {
+      const options = this.prepareOptions(
+        host,
+        port,
+        path,
+        HttpVerb.DELETE,
+      );
+
+      const req 
+        = http.request(options, (res) => {
+        let data: any = []
+
+        if(res.statusCode !== 200) {
+          throw new Error("failed to DELETE");
+        }
+
+        res.on('data', chunk => {
+          data.push(chunk);
+        });
+
+        res.on('end', () => {
+          console.log(Buffer.concat(data).toString());
+
+          const result: T = JSON.parse(Buffer.concat(data).toString())
+
+          console.log('DELETE results:', result);
+
+          resolve(result);
+        });
+      });
+
+      try {
+        req.end();
+      } catch (e: any) {
+        console.error(e);
+      }
+    });
+  }
+
+  private prepareOptions(
+    host: string,
+    port: number,
+    path: any,
+    method: HttpVerb, 
+    contentLength?: number
+  ): HttpClientOptions {
+    const headers = contentLength ?
+      {
+        'Content-Type': 'application-json',
+        'Content-Length': contentLength,
+      }
+    
+    :
+      {
+        'Content-Type': 'application-json',
+      }
+    ;
+
+    return  {
+      host,
+      port,
+      path,
+      method,
+      headers
+    };
   }
 };
